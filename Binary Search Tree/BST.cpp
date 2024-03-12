77d527e5509bdeb04e52200084a39bfede44c7ac
Saim Ahmed Emon <81570237+EmonThePrince@users.noreply.github.com>
2024-03-12 11:15:16 +0600

    Update BST.cpp

--- Binary Search Tree/BST.cpp
+++ Binary Search Tree/BST.cpp
@@ -1,33 +1,34 @@
 #include <bits/stdc++.h>  
  
 using namespace std;
- 
-int bst[101];
+
+const int MAX_SIZE = 1e6;
+int bst[MAX_SIZE];
  
 void insert(int data){
     if(bst[1] == 0) {bst[1] = data; return;}
     int root = 1;
-    while(root >= 101 || bst[root] != 0){
+    while(root < MAX_SIZE && bst[root] != 0){
         if(data < bst[root]){
-        //left
-        root = 2*root;
+            //left
+            root = 2*root;
         }
         else{
             //right
             root = 2*root + 1; 
         }
     }
-    if(root<101) bst[root] = data;
+    if(root<MAX_SIZE) bst[root] = data;
 }
 
 int search(int data){
     if(bst[1] == 0) {return -1;}
     int root = 1;
-    while(root >= 101 || bst[root] != 0){
+    while(root < MAX_SIZE && bst[root] != 0){
         if(data == bst[root]) return root;
         if(data < bst[root]){
-        //left
-        root = 2*root;
+            //left
+            root = 2*root;
         }
         else{
             //right
@@ -39,16 +40,22 @@ int search(int data){
  
 int main()
 {
+    //inserting data..
     insert(5);
     insert(10);
     insert(3);
     insert(11);
     insert(4);
     insert(2);
+    
+    //searching data...
     cout<<search(4)<<endl;
     cout<<search(11)<<endl;
     cout<<search(12)<<endl;
+    
+    //printing first 10 data in the tree...
     for(int i = 0; i < 10; i++ ) cout<<bst[i]<<" "; cout<<endl;
+    for(int i = 0; i < 10; i++ ) cout<<i<<" "; cout<<endl;
     
     return 0;
 }
