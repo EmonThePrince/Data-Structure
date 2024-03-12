dc01c30d72a8233f08fb07333326c50eea941ac6
Saim Ahmed Emon <81570237+EmonThePrince@users.noreply.github.com>
2024-03-12 11:27:40 +0600

    Update BST.cpp

--- Binary Search Tree/BST.cpp
+++ Binary Search Tree/BST.cpp
@@ -38,7 +38,25 @@ int search(int data){
     }
     return -1;
 }
- 
+
+int insertOrSearch(int data){
+    if(bst[1] == 0) {bst[1] = data; return -1;}
+    int root = 1;
+    while(root < MAX_SIZE && bst[root] != 0){
+        if(data == bst[root]) return root;
+        if(data < bst[root]){
+            //left
+            root = 2*root;
+        }
+        else{
+            //right
+            root = 2*root + 1; 
+        }
+    }
+    if(root<MAX_SIZE) bst[root] = data;
+    return -1;
+} 
+
 int main()
 {
     //inserting data..
@@ -54,10 +72,10 @@ int main()
     cout<<search(4)<<endl;
     cout<<search(11)<<endl;
     cout<<search(12)<<endl;
+    cout<<insertOrSearch(12)<<endl;
     
     //printing first 10 data in the tree...
-    for(int i = 0; i < 10; i++ ) cout<<bst[i]<<" "; cout<<endl;
-    for(int i = 0; i < 10; i++ ) cout<<i<<" "; cout<<endl;
+    for(int i = 0; i < 35; i++ ) cout<<bst[i]<<" "; cout<<endl;
     
     return 0;
 }
