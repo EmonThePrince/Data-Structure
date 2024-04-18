09277bcde443f2341515ddb7c278dfa39765f082
Saim Ahmed Emon <81570237+EmonThePrince@users.noreply.github.com>
2024-04-18 06:57:32 +0000

    warshal3

--- Graph/WarshallAlgorithm.cpp
+++ Graph/WarshallAlgorithm.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 
 int MAX = 100;
+
 bool check(string s, int n){
+    int c = 0;
     for(int i = 0; i<s.size(); i++){
         if(s[i] == ('0' + n)) return false;
     }
@@ -11,23 +13,19 @@ bool check(string s, int n){
 
 vector<vector<string>> warshallAlgo(int n, vector<vector<int>> &w){
     vector<vector<string>> paths(n,vector<string> (n));
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            paths[i][j].push_back('0' + i);
-        }
-        cout<<endl;
-    }
+    // for(int i = 0; i < n; i++){
+    //     for(int j = 0; j < n; j++){
+    //         paths[i][j].push_back('0' + i);
+    //     }
+    //     cout<<endl;
+    // }
     for(int k = 0; k < n; k++){
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
-                if(w[i][j]>w[i][k]+w[k][j]){
-                    if(check(paths[i][j],k))paths[i][j][paths[i][j].size()-1] = ('0'+k);
-                    if(check(paths[i][j],j)) paths[i][j].push_back('0' + j);
+                if(w[i][j]>w[i][k]+w[k][j] && w[i][k]!= MAX && w[k][j] != MAX){
+                    if(check(paths[i][j],k))paths[i][j].push_back('0'+k);
+                    w[i][j] = w[i][k] + w[k][j];
                 }
-                else{
-                    if(w[i][j] != MAX && check(paths[i][j],j)) paths[i][j].push_back('0' + j);
-                }
-                w[i][j] = min(w[i][j], w[i][k]+w[k][j]);
             }
         }
     }
@@ -37,10 +35,10 @@ vector<vector<string>> warshallAlgo(int n, vector<vector<int>> &w){
 
 int main(){
     vector<vector<int>> w ={
-        {2, 7, 5, 10},
-        {MAX, MAX, MAX, 2},
-        {MAX, MAX, MAX, MAX},
-        {MAX, MAX, MAX, MAX}
+        {7, 5, MAX, MAX},
+        {7, MAX, MAX, 2},
+        {MAX, 3, MAX, MAX},
+        {4, MAX, 1, MAX}
     };
     vector<vector<string>> paths = warshallAlgo(4, w);
     for(int i = 0; i < 4; i++){
@@ -50,7 +48,7 @@ int main(){
         }
         cout<<endl;
     }
-
+    cout<<"paths"<<endl;
     for(int i = 0; i < 4; i++){
         for(int j = 0; j < 4; j++){
             cout<<paths[i][j]<<" ";
